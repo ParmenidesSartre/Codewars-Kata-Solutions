8039d96d056af7023766646d2fd6d1ba7d8fd67d
Mohd Faizal Bin Azman <tnemovich@gmail.com>
2019-11-04 08:29:18 +0800

    commits

---
+++ 8 Kyu/Python/BasicMathematicalOperations.py
@@ -0,0 +1,9 @@
+def basic_op(operator, value1, value2):
+    if operator == '+':
+        return value1 + value2
+    else if operator == '-':
+        return value1 - value2
+    else if operator == '*':
+        return value1 * value2
+    else:
+        return value1 / value2
